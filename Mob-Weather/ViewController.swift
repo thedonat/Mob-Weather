b5d590a3b553927f546efc2e4b8d3fd58c972e2d
Burak Donat <burakdonat@gmail.com>
2020-09-10 22:58:53 +0300

    first commit

---
+++ Mob-Weather/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Mob-Weather
+//
+//  Created by Burak Donat on 10.09.2020.
+//  Copyright © 2020 Burak Donat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
